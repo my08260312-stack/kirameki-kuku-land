ee62078d46f9139f3b737ac87700cc757b9fb5bc
my08260312-stack <my08260312@gmail.com>
2026-02-25 13:48:31 +0900

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kirameki-kuku-land/sw.js', { scope: '/kirameki-kuku-land/' })})}
